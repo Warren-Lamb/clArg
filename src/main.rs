787bc5df4f36d4e3153d84766af206ef8fe762d8
Warren-Lamb <10warren10@gmail.com>
2019-06-04 19:03:01 -0400

    initial test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::process;
+use std::env;
+
 fn main() {
     println!("Hello, wod!");
+}
+
+mod tests {
+	extern crate assert_cmd;
+
+	use tests::assert_cmd::prelude::*;
+	use std::process::Command;
+
+	#[test]
+	fn config_valid_args(){
+		let mut cmd = Command::cargo_bin("clarg").unwrap();
+		cmd.arg("r2d2")
+		   .assert()
+		   .success();
+	}	
 }
