bd130256ddf030cada9b106fbd5cc5623952cec0
Warren-Lamb <10warren10@gmail.com>
2019-06-09 02:43:21 -0400

    finish err msg and pass tests in rxsx fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,39 @@
 use std::error::Error;
 
 #[derive(Debug)]
-pub struct  Config {
-	pub arg1: u32,
-	pub arg2: u32,
+pub struct Config {
+    pub arg1: u32,
+    pub arg2: u32,
+}
+
+fn remove_first(s: &str) -> Option<&str> {
+    s.chars().next().map(|c| &s[c.len_utf8()..])
 }
 
 impl Config {
-	pub fn new (mut args: std::env::Args) -> Result<Config, &'static str> {
-		args.next();
-		let availArgs = match args.next() {
-			Some(arg) => arg,
-			None => return Err("No arguments provided"),
-		};
-		let argParts: Vec<&str> = availArgs.split("d").collect();
-		if argParts.len() != 2  { 
-			return Err("Must be in the form r<rolls>s<sides>");
-		}
-		let mut argParts = argParts.iter();
-		let parseErr = "Must be in format rxdx where x > 0";
-		let r = argParts.next().unwrap().parse::<u32>();
-		let s = argParts.next().unwrap().parse::<u32>();
-		if r.is_err() || s.is_err() {
-			return Err(parseErr);
-		}
-		let r = r.unwrap();
-		let s = s.unwrap();
-		if r ==0 || s ==0 {
-			return Err(parseErr);
-		}
-		Ok(Config{arg1: r,arg2: s})
-	}
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
+        let availArgs = match args.next() {
+            Some(arg) => arg,
+            None => return Err("No arguments provided"),
+        };
+        let argParts: Vec<&str> = availArgs.split("s").collect();
+        if argParts.len() != 2 {
+            return Err("Must be in the form r<rolls>s<sides>");
+        }
+        let mut argParts = argParts.iter();
+        let parseErr = "Must be in format rxsx where x > 0";
+        let r = argParts.next().unwrap();
+        let r = remove_first(r).unwrap().parse::<u32>();
+        let s = argParts.next().unwrap().parse::<u32>();
+        if r.is_err() || s.is_err() {
+            return Err(parseErr);
+        }
+        let r = r.unwrap();
+        let s = s.unwrap();
+        if r == 0 || s == 0 {
+            return Err(parseErr);
+        }
+        Ok(Config { arg1: r, arg2: s })
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,40 @@
-use std::process;
 use std::env;
+use std::process;
 extern crate clArg;
 use clArg::Config;
 
-
 fn main() {
-	let config = Config::new(env::args()).unwrap_or_else(|err| {
-		eprintln!("Problem parsing arguments: {}", err);
-	    process::exit(1);
-     });
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    println!("{:?}",config);
 }
 
 mod tests {
-	extern crate assert_cmd;
+    extern crate assert_cmd;
 
-	use tests::assert_cmd::prelude::*;
-	use std::process::Command;
+    use std::process::Command;
+    use tests::assert_cmd::prelude::*;
 
-	#[test]
-	fn config_valid_args(){
-		let mut cmd = Command::cargo_bin("clarg").unwrap();
-		cmd.arg("r2d2")
-		   .assert()
-		   .success();
-	}	
-	#[test]
-	fn config_empty_args(){
-		let mut cmd = Command::cargo_bin("clarg").unwrap();
-		cmd.assert()
-			.failure()
-			.stderr("Problem parsing arguments: No arguments provided\n");
-	}
-	#[test]
-	fn config_nonZero_args(){
-		let mut cmd = Command::cargo_bin("clarg").unwrap();
-		cmd.arg("r0d0")
-			.assert()
-			.failure()
-			.stderr("Problem parsing argments: Must be in format rxdx where x > 0\n");
-	}
+    #[test]
+    fn config_valid_args() {
+        let mut cmd = Command::cargo_bin("clarg").unwrap();
+        cmd.arg("r2s2").assert().success();
+    }
+    #[test]
+    fn config_empty_args() {
+        let mut cmd = Command::cargo_bin("clarg").unwrap();
+        cmd.assert()
+            .failure()
+            .stderr("Problem parsing arguments: No arguments provided\n");
+    }
+    #[test]
+    fn config_nonZero_args() {
+        let mut cmd = Command::cargo_bin("clarg").unwrap();
+        cmd.arg("r0s0")
+            .assert()
+            .failure()
+            .stderr("Problem parsing arguments: Must be in format rxsx where x > 0\n");
+    }
 }
